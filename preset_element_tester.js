71acf8ae39b6257646a7b059878c9ddd0a436dca
jnlm <jl@jnlm.info>
2020-04-15 10:24:59 -0700

    more water flow tests

--- preset_element_tester.js
+++ preset_element_tester.js
@@ -11,6 +11,19 @@ function element_test_setup(){
     cells[cx][cy + 1].element.water.level.set(4.0);
     cells[cx + 1][cy].element.water.level.set(4.0);
 
+    cx += 10
+    cells[cx][cy].element.water.level.set(4.0);
+    cells[cx + 2][cy].element.water.level.set(4.0);
+
+    cx += 10
+    cells[cx][cy].element.water.level.set(4.0);
+    cells[cx + 5][cy].element.water.level.set(4.0);
+
+    cx += 15
+    cells[cx][cy].element.water.level.set(4.0);
+    cells[cx][cy + 6].element.water.level.set(4.0);
+    cells[cx + 3][cy + 3].element.water.level.set(4.0);
+
     flip_cells();
     draw();
 }
